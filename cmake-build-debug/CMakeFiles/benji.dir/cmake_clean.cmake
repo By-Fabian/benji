b316059954c5ca92e7ba29c4a7adc95d84e00106
By-Fabian <yuri.vilela@utec.edu.pe>
2019-10-18 16:51:18 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/benji.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/benji.dir/main.cpp.o"
+  "benji"
+  "benji.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/benji.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
